caf3023dc02fb3d40621b55ea0e2fcb34de1241f
Joel McCoy <nbajoel@gmail.com>
2023-12-02 19:53:29 -0600

    working examples

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # boto3-async-experiments
 Experiments/performance testing with boto3 and async.
+
+## Setup
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Run Performance Test
+The performance tests use the s3 ListBuckets call.  The test will run the call 100 times and then run the test 3 times.  The results will be printed to the console.
+
+You can modify the `src/main.py` file to change the number of times to run each test and the number of times to run the s3 ListBuckets call.
+
+```bash
+python3 src/main.py
+```
+
+## Results
+Sample of results on my local machine (Apple M2 Pro with 16GB RAM)
+
+100 runs of the s3 ListBuckets call and 3 runs of the test
+```
+synchronous elapsed time for run 0: 7.94390033298987 seconds
+synchronous elapsed time for run 1: 7.485672875016462 seconds
+synchronous elapsed time for run 2: 7.424951791006606 seconds
+async boto3 elapsed time for run 0: 0.8219816250202712 seconds
+async boto3 elapsed time for run 1: 0.8033592499850783 seconds
+async boto3 elapsed time for run 2: 0.8520164999936242 seconds
+async aioboto3 elapsed time for run 0: 1.4005139590008184 seconds
+async aioboto3 elapsed time for run 1: 1.4623616670141928 seconds
+async aioboto3 elapsed time for run 2: 1.4225388749910053 seconds
+Average synchronous elapsed time: 7.6181749996709796 seconds
+Average async boto3 elapsed time: 0.8257857916663246 seconds
+Average async aioboto3 elapsed time: 1.4284715003353388 seconds
+```
+
+## Conclusion
+* The async boto3 option is faster than the synchrous boto3 option (about 9x faster).  
+* The async aioboto3 option is still faster than the synchronous boto3 option (about 5.3x faster).
+
+When running a large number of boto3 calls that can be run concurrently, it is worth using the async boto3 option to speed up the execution time.
---
+++ src/main.py
@@ -0,0 +1,82 @@
+import asyncio
+import functools
+from time import perf_counter
+
+import aioboto3
+import boto3
+
+# The ammount of calls to make to list_buckets
+AMOUNT_OF_CALLS = 1
+TIMES_TO_RUN_PERFORMANCE_TEST = 100
+
+
+def synchronous_example() -> float:
+    """Tests the performance of the boto3 client used synchronously."""
+    client = boto3.client("s3")
+    start_time = perf_counter()
+    # Make 20 calls to list_buckets
+    for _ in range(AMOUNT_OF_CALLS):
+        client.list_buckets()
+    end_time = perf_counter()
+    return end_time - start_time
+
+
+async def async_aiboto3_example():
+    session = aioboto3.Session()
+    async with session.client("s3") as s3:
+        start_time = perf_counter()
+        await asyncio.gather(*[s3.list_buckets() for _ in range(AMOUNT_OF_CALLS)])
+        end_time = perf_counter()
+        return end_time - start_time
+
+
+async def async_boto3_example() -> float:
+    client = boto3.client("s3")
+    loop = asyncio.get_running_loop()
+
+    start_time = perf_counter()
+    await asyncio.gather(
+        *[
+            loop.run_in_executor(None, functools.partial(client.list_buckets))
+            for _ in range(AMOUNT_OF_CALLS)
+        ]
+    )
+    end_time = perf_counter()
+    return end_time - start_time
+
+
+def main():
+    """Entry point of the application."""
+    sync_elapsed_time_total = 0
+    async_boto3_elapsed_time_total = 0
+    async_aiboto3_elapsed_time_total = 0
+
+    # Run the synchronous example multiple times to get an average
+    for i in range(TIMES_TO_RUN_PERFORMANCE_TEST):
+        elapsed_time = synchronous_example()
+        sync_elapsed_time_total += elapsed_time
+        print(f"synchronous elapsed time for run {i}: {elapsed_time} seconds")
+
+    for i in range(TIMES_TO_RUN_PERFORMANCE_TEST):
+        elapsed_time = asyncio.run(async_boto3_example())
+        async_boto3_elapsed_time_total += elapsed_time
+        print(f"async boto3 elapsed time for run {i}: {elapsed_time} seconds")
+
+    for i in range(TIMES_TO_RUN_PERFORMANCE_TEST):
+        elapsed_time = asyncio.run(async_aiboto3_example())
+        async_aiboto3_elapsed_time_total += elapsed_time
+        print(f"async aioboto3 elapsed time for run {i}: {elapsed_time} seconds")
+
+    print(
+        f"Average synchronous elapsed time: {sync_elapsed_time_total / TIMES_TO_RUN_PERFORMANCE_TEST} seconds"
+    )
+    print(
+        f"Average async boto3 elapsed time: {async_boto3_elapsed_time_total / TIMES_TO_RUN_PERFORMANCE_TEST} seconds"
+    )
+    print(
+        f"Average async aioboto3 elapsed time: {async_aiboto3_elapsed_time_total / TIMES_TO_RUN_PERFORMANCE_TEST} seconds"
+    )
+
+
+if __name__ == "__main__":
+    main()
